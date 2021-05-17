55ba0d1dd18549cf3e9a18a1e0f417b5c9e17a57
Thomas Wilmot <thomaswilmot@gmail.com>
2021-05-17 18:09:55 +1000

    Added application name to Application

--- saturnv/ui/application.py
+++ saturnv/ui/application.py
@@ -4,7 +4,9 @@ from Qt.QtCore import Qt
 
 class Application(QApplication):
 
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setApplicationName('SaturnV Launcher')
 
 
 Application.setAttribute(Qt.AA_EnableHighDpiScaling)
