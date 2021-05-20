f01f37dd926858d9be75291c02021411149a9d2e
Thomas Wilmot <thomaswilmot@gmail.com>
2021-05-20 22:43:16 +1000

    Removed tables

--- saturnv/database/__init__.py
+++ saturnv/database/__init__.py
@@ -2,7 +2,4 @@ from saturnv.database import core
 
 from saturnv.database.presets import Preset, Version, Setting
 from saturnv.database.presets import Shortcut, Override
-
 from saturnv.database.shelves import Shelf
-
-from saturnv.database import manage
